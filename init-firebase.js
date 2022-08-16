69bee8317f66a4c0ec9f141eb1db28d77c3337bc
Rehan <bajwarehan14@gmail.com>
2022-08-16 14:01:18 -0400

    SUMMARIZE YOUR CHANGES IN QUOTES

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIzBRW4hiGawAEI9CFSmgmR2X3L_N_QMU",
+  authDomain: "fir-day-bab46.firebaseapp.com",
+  projectId: "fir-day-bab46",
+  storageBucket: "fir-day-bab46.appspot.com",
+  messagingSenderId: "17038089371",
+  appId: "1:17038089371:web:4e9a6abcf511f47fd4fa3e",
+  measurementId: "G-Y7T6YV4GYT"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let nextButton = document.getElementById("nextButton");
 let previousButton = document.getElementById("previousButton");
 let arrowtrans = document.getElementById("og");
+let notesInput = document.getElementById("input");
 
 // nextButton.onclick=function(event){
 //     event.preventDefault();
@@ -27,3 +28,16 @@ arrowtrans.onmouseover = function() {
 arrowtrans.onmouseout = function() {
     arrowtrans.src='images/arrow-back.png';
 }
+
+function updateDB(event){
+    //prevents page from refreshing
+    event.preventDefault()
+
+    const entry = {
+        note: notesInput.value,
+    }
+
+    database.push(entry);
+    notesInput.value="";
+}
+
